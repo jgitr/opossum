0123a43314e9f37560a413e292a203a7c7e21482
Tobi <tobias.krebs@posteo.de>
2019-08-13 13:09:23 +0200

    setting up main with defaults

--- src/main.py
+++ src/main.py
@@ -3,15 +3,15 @@ from opossum import UserInterface
 
 
 if __name__ == "__main__":
-    u = UserInterface(1000, 100, seed=12, categorical_covariates = None)
+    u = UserInterface(10000, 50, seed=None, categorical_covariates = None)
                       # If wanted, replace None with either: int, list of 2 
                       # ints, list of 1 int and 1 list of ints, where 
                       # [num of covariates, num of categories]
-    u.generate_treatment(random_assignment = False, 
-                         assignment_prob = 'low', 
-                         constant_pos = False, 
+    u.generate_treatment(random_assignment = True, 
+                         assignment_prob = 0.5, 
+                         constant_pos = True, 
                          constant_neg = False,
-                         heterogeneous_pos = True, 
+                         heterogeneous_pos = False, 
                          heterogeneous_neg = False, 
                          no_treatment = False, 
                          discrete_heterogeneous = False,
