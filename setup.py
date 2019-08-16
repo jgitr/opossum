1bafedb41810c748c26eaa66913be2c0248dc15e
lax <winjules2@gmail.com>
2019-08-16 20:07:26 +0200

    setup url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='opossum',
     version='0.0.1',
     description='Simulated Data Generating Process',
-    url='git@github.com:jgitr/opossum.git',
+    url='https://github.com/jgitr/opossum',
     author='Tobias Krebs, Julian Winkel',
     author_email='julian.winkel@hu-berlin.de',
     license='MIT',
