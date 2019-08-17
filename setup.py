251b6bc68302ce5b205262d33c3aeb7116dfc387
lax <winjules2@gmail.com>
2019-08-17 12:23:19 +0200

    econometrics website as homepage

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='opossum',
     version='0.0.1',
     description='Simulated Data Generating Process',
+    url='https://humboldt-wi.github.io/blog/research/',
     author='Tobias Krebs, Julian Winkel',
     author_email='julian.winkel@hu-berlin.de',
     license='MIT',
